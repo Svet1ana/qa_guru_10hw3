71e3590e084584c85610818b8a693757bfffda76
Svetlana Belousova <belousova@art2soft.ru>
2022-07-04 12:14:46 +0300

    added: selenide test/

---
+++ src/test/java/SelenideTest.java
@@ -0,0 +1,27 @@
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void selenideTest () {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+//        element(byId("wiki-tab")).click();
+        $(".markdown-body").$(byText("Soft assertions")).shouldBe(visible).click();
+        $("#user-content-3-using-junit5-extend-test-class").parent().shouldHave(text("Using JUnit5 extend test class"));
+    }
+}
